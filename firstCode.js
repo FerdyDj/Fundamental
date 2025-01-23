b1d8a60ab43bff9bfd8a7e6da10774dbec83210f
Ferdy <ferdydjasni@gmail.com>
2025-01-23 09:49:27 +0700

    first code

---
+++ firstCode.js
@@ -0,0 +1,23 @@
+// buat menampilkan nilai di terminal
+console.log("Hello Purwadhika!")
+
+/*
+comment
+2 baris atau lebih
+baris ke 3
+*/
+
+let message // membuat variable bernama message
+message = "Hello" // mengisi variable message dengan sebuah nilai
+console.log(message)
+
+var globalVariable = "Hello World!"
+
+let replaceable = "This value will be replaced"
+replaceable = "I love Javascript"
+
+const constant = "Hello Purwadhika!"
+
+console.log(globalVariable)
+console.log(replaceable)
+console.log(constant)
